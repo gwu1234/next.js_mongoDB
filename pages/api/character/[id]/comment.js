b0f77655d0fb686a6210ac69e1c514c880717315
user <user@users-MacBook-Pro.local>
2020-09-30 09:16:32 -0400

    finish

--- pages/api/character/[id]/comment.js
+++ pages/api/character/[id]/comment.js
@@ -11,19 +11,16 @@ export default async (req, res) => {
 
     console.log ("id at api = ", id);
     console.log ("method = ", method);
-    //console.log ("_id = ", _id);
-
+    
     switch (method) {
         case 'GET':
             try {
                 console.log("at api/[id].comment.js GET")
-                //const comment = await Comment.findById({id: parseInt(id)});
                 const comment = await Comment.find();
                 if (!comment) {
                     console.log("not found")
                     return res.status(400).json({ success: false });
                 }
-                //console.log(comment)
                 res.status(200).json({ success: true, data: comment });
             } catch (error) {
                 console.log(error)
@@ -45,12 +42,10 @@ export default async (req, res) => {
         case 'PUT':
             try {
                 console.log("at api/[id].comment.js PUT")
-                //let body = JSON.parse(req.body)
-                //_id  = body._id
                 console.log("id = ", id)
                 const comment = await Comment.findByIdAndUpdate(id, req.body, {
                     new: true,
-                    runValidators: false,
+                    runValidators: true,
                     useFindAndModify: false
                 });
 
